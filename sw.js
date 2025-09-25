505e7ff8a6609bc6c26944c3757c252fd23f543f
stephaneb59-rgb <stephaneb59@gmail.com>
2025-09-25 12:57:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("nihss-cache").then(cache => {
+      return cache.addAll([
+        "/nihss_mobile_app SB.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
